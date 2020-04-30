415d2ef340fa197f9318c13acc35344b70cdab60
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-04-30 15:08:39 +0530

    created a function to print the menu list

--- numbers.c
+++ numbers.c
@@ -1,4 +1,19 @@
-int main(void)
-{
+#include<stdio.h>
+#include "list.h"
+#include "numbers.h"
+
+void print_menu(void) {
+  printf("Main Menu\n");
+  printf("---------\n");
+  char menu[MENU_LENGTH][85] = MENU;
+
+  for (int index = 0; index < MENU_LENGTH; index++) {
+    printf("%s\n", menu[index]);
+  }
+  printf("Please enter the alphabet of the operation you would like to perform\n");
+}
+
+int main(void) {
+  print_menu();
   return 0;
 }
---
+++ numbers.h
@@ -0,0 +1,22 @@
+#ifndef __NUMBERS_H_
+#define __NUMBERS_H_
+
+#define MENU_LENGTH 13
+
+#define MENU {\
+  "(a) add a number to the end of the list",\
+  "(b) add a number to the start of the list",\
+  "(c) insert a number at a given position in the list",\
+  "(d) add a unique item on the list at the end(if it alreay exists, do not insert)",\
+  "(e) remove a number from the beginning of the lists",\
+  "(f) remove a number from the end of the list",\
+  "(g) remove a number from a given position in the list",\
+  "(h) remove first occurrence of a number",\
+  "(i) remove all occurrences of a number",\
+  "(j) clear the whole list",\
+  "(k) check if a number exists in the list",\
+  "(l) display the list of numbers",\
+  "(m) exit",\
+}
+
+#endif
